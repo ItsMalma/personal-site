d0df9842416d445abe29f5540119f4a357ab2c22
ItsMalma <itsmalma28@gmail.com>
2023-11-01 01:59:00 +0700

    feat(components): install and setup shadcn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+export const fontSans = Inter({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div id="root" className={fontSans.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
